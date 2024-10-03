933acef7f261df174143e1cfbee3e80f94ac202c
MuhammadNajeebAkram <Mnajeeb_Akram@hotmail.com>
2024-10-03 21:19:26 +0500

    disable and enable react/no-unescaped-entities in index.js file

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,13 +16,17 @@ export default function Home() {
         <Typography sx={{color: '#fff', fontSize: 18}}>
 
       
-      
-      Welcome to Pakistan Past Papers, your one-stop resource for aced exams! We understand the importance of thorough preparation, and that's why we provide a comprehensive collection of past papers from various Pakistani educational boards. Whether you're a student striving for top grades, a teacher seeking effective revision materials, or a parent supporting your child's academic journey, Pakistan Past Papers is here for you.
-      
-      
-      
-Navigate seamlessly through our user-friendly platform to find past papers for all major subjects across different classes and boards. Download them easily and utilize them to practice, test your knowledge, and identify areas for improvement. With Pakistan Past Papers as your study partner, you'll be well-equipped to tackle your exams with confidence!
+        {/* eslint-disable react/no-unescaped-entities */}
+        <p>
+  Welcome to Pakistan Past Papers, your one-stop resource for aced exams! We understand the importance of thorough preparation, and that\'s why we provide a comprehensive collection of past papers from various Pakistani educational boards. Whether you\'re a student striving for top grades, a teacher seeking effective revision materials, or a parent supporting your child\'s academic journey, Pakistan Past Papers is here for you.
 
+  <br />
+
+  <br />
+
+  Navigate seamlessly through our user-friendly platform to find past papers for all major subjects across different classes and boards. Download them easily and utilize them to practice, test your knowledge, and identify areas for improvement. With Pakistan Past Papers as your study partner, you'll be well-equipped to tackle your exams with confidence!
+</p>
+{/* eslint-enable react/no-unescaped-entities */}
 </Typography>
       </Box>
 
