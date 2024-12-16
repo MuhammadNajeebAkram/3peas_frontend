dfbefa31d57b4e3c46dbbf926fae633d7920dcf9
MuhammadNajeebAkram <mnajeeb_akram@hotmail.com>
2024-12-16 11:19:57 +0500

    fix font issues

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,11 +4,23 @@ import { DataProvider } from '@/context/DataContext';
 import Head from 'next/head';
 import { GoogleTagManager } from '@next/third-parties/google'; // Import the GoogleTagManager component
 import Script from 'next/script';
+import { Noto_Nastaliq_Urdu, Roboto_Serif } from 'next/font/google';
+
+const notoNastaliqUrdu = Noto_Nastaliq_Urdu({
+  subsets: ['latin'],
+  weight: '400',
+});
+
+const robotoSerif = Roboto_Serif({
+  subsets: ['latin'],
+  weight: '100',
+  style: ['normal', 'italic'],
+});
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-   
+    <div className={`${notoNastaliqUrdu.className} ${robotoSerif.className}`}>
 
   <Layout class_id={pageProps.class_id} class_name={pageProps.class_name} subject_id = {pageProps.subject_id}
   subject_name = {pageProps.subject_name} board_id = {pageProps.board_id} 
@@ -16,7 +28,7 @@ export default function App({ Component, pageProps }) {
     
   <Component {...pageProps} />
   </Layout>
- 
+  </div>
   </>
   
 
